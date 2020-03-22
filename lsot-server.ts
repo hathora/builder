23f4d5eda277d620796262dba3f814638d4d7e2e
Harsh Pandey <hpandey793@gmail.com>
2020-03-22 02:06:14 -0400

    more server impl

--- lsot-server.ts
+++ lsot-server.ts
@@ -1,49 +1,72 @@
 import socketio from "socket.io";
 import impl from "./lsot-impl";
 
-const users = {};
-const states = {};
+const users: Map<string, any> = new Map();
+const states: Map<string, any> = new Map();
+const connections: Map<string, Set<SocketIO.Socket>> = new Map();
 
-socketio(3000).on("connection", socket => {
-  socket.on("registerUser", (userData, callback) => {
-    const userId = Math.random()
-      .toString(36)
-      .substring(2);
-    users[userId] = userData;
-    callback(userId);
-  });
-  socket.on("new", userId => {
-    const stateId = Math.random()
-      .toString(36)
-      .substring(2);
-    const userData = users[userId];
-    const state = impl.createGame(userData);
-    states[stateId] = state;
+const io = socketio(3000);
+
+io.on("connection", socket => {
+  const stateId = socket.handshake.query.stateId;
+  const userId = socket.handshake.query.userId;
+  addConnection(stateId, socket);
+  socket.on("disconnect", () => {
+    deleteConnection(stateId, socket);
   });
 
-  socket.on("joinGame", (userId, stateId) => {
-    const state = states[stateId];
-    const userData = users[userId];
+  socket.on("joinGame", () => {
+    const state = states.get(stateId);
+    const userData = users.get(userId);
     impl.joinGame(state, userData);
+    broadcastUpdates(stateId, state);
   });
-  socket.on("startGame", (userId, stateId, roleList, playerOrder) => {
-    const state = states[stateId];
-    const userData = users[userId];
+  socket.on("startGame", (roleList, playerOrder) => {
+    const state = states.get(stateId);
+    const userData = users.get(userId);
     impl.startGame(state, userData, roleList, playerOrder);
+    broadcastUpdates(stateId, state);
   });
-  socket.on("proposeQuest", (userId, stateId, questId, proposedMembers) => {
-    const state = states[stateId];
-    const userData = users[userId];
+  socket.on("proposeQuest", (questId, proposedMembers) => {
+    const state = states.get(stateId);
+    const userData = users.get(userId);
     impl.proposeQuest(state, userData, questId, proposedMembers);
+    broadcastUpdates(stateId, state);
   });
-  socket.on("voteForProposal", (userId, stateId, questId, vote) => {
-    const state = states[stateId];
-    const userData = users[userId];
+  socket.on("voteForProposal", (questId, vote) => {
+    const state = states.get(stateId);
+    const userData = users.get(userId);
     impl.voteForProposal(state, userData, questId, vote);
+    broadcastUpdates(stateId, state);
   });
-  socket.on("voteInQuest", (userId, stateId, questId, vote) => {
-    const state = states[stateId];
-    const userData = users[userId];
+  socket.on("voteInQuest", (questId, vote) => {
+    const state = states.get(stateId);
+    const userData = users.get(userId);
     impl.voteInQuest(state, userData, questId, vote);
+    broadcastUpdates(stateId, state);
   });
 });
+
+function addConnection(stateId: string, socket: socketio.Socket) {
+  if (!connections.has(stateId)) {
+    connections.set(stateId, new Set([socket]));
+  } else {
+    connections.get(stateId).add(socket);
+  }
+}
+
+function deleteConnection(stateId: string, socket: SocketIO.Socket) {
+  connections.get(stateId).delete(socket);
+  if (connections.get(stateId).size === 0) {
+    connections.delete(stateId);
+  }
+}
+
+function broadcastUpdates(stateId: string, state) {
+  connections.get(stateId).forEach(socket => {
+    const userId = socket.handshake.query.userId;
+    const userData = users.get(userId);
+    const userState = impl.getUserState(state, userData);
+    socket.emit("state", userState);
+  });
+}
