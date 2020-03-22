59f1aa2fd20d84e1de54c36c5371c13b5929ac0e
Harsh Pandey <hpandey793@gmail.com>
2020-03-21 23:18:29 -0400

    more formatting

--- lsot-server-common.ts
+++ lsot-server-common.ts
@@ -1,39 +1,39 @@
 import {
-    UserId,
-    QuestId,
-    PlayerName,
-    Role,
-    Vote,
-    PlayerState,
-    PlayerData
+  UserId,
+  QuestId,
+  PlayerName,
+  Role,
+  Vote,
+  PlayerState,
+  PlayerData
 } from "./lsot-types";
 
 export interface AbstractImpl<T> {
-    createGame(playerData: PlayerData): T;
-    joinGame(state: T, playerData: PlayerData): void;
-    startGame(
-        state: T,
-        playerData: PlayerData,
-        roleList: Role[],
-        playerOrder: PlayerName[]
-    ): void;
-    proposeQuest(
-        state: T,
-        playerData: PlayerData,
-        questId: QuestId,
-        proposedMembers: PlayerName[]
-    ): void;
-    voteForProposal(
-        state: T,
-        playerData: PlayerData,
-        questId: QuestId,
-        vote: Vote
-    ): void;
-    voteInQuest(
-        state: T,
-        playerData: PlayerData,
-        questId: QuestId,
-        vote: Vote
-    ): void;
-    getUserState(state: T, playerData: PlayerData): PlayerState;
+  createGame(playerData: PlayerData): T;
+  joinGame(state: T, playerData: PlayerData): void;
+  startGame(
+    state: T,
+    playerData: PlayerData,
+    roleList: Role[],
+    playerOrder: PlayerName[]
+  ): void;
+  proposeQuest(
+    state: T,
+    playerData: PlayerData,
+    questId: QuestId,
+    proposedMembers: PlayerName[]
+  ): void;
+  voteForProposal(
+    state: T,
+    playerData: PlayerData,
+    questId: QuestId,
+    vote: Vote
+  ): void;
+  voteInQuest(
+    state: T,
+    playerData: PlayerData,
+    questId: QuestId,
+    vote: Vote
+  ): void;
+  getUserState(state: T, playerData: PlayerData): PlayerState;
 }
