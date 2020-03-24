a9c060138c9a955736fa4b7f634eaa04718f559e
Harsh Pandey <hpandey793@gmail.com>
2020-03-23 21:31:19 -0400

    client codegen

--- generate.ts
+++ generate.ts
@@ -1,23 +1,34 @@
 import { safeLoad } from "js-yaml";
 import { readFileSync, writeFileSync, existsSync, mkdirSync } from "fs";
 
-const stringifyType = (key, value) => {
+const stringifyType = (key: string, value: any) => {
   if (Array.isArray(value)) {
     return `export enum ${key} {\n  ${value.join(",\n  ")}\n}`;
   } else if (typeof value === "object") {
     const parts = Object.entries(value).map(([k, v]) => `${k}: ${v};`);
-    return `export interface ${key} {\n  ${parts.join("\n  ")};\n}`;
+    return `export interface ${key} {\n  ${parts.join("\n  ")}\n}`;
   } else {
     return `export type ${key} = ${value};`;
   }
 };
 
-const stringifyMethod = (key, value) => {
+const stringifyServerMethod = (key: string, value: any) => {
   const args = value ? Object.keys(value) : [];
-  return `  socket.on("${key}", (${args.join(', ')}) => {
-    impl.${key}(state, userData${args.length ? [''].concat(args).join(', ') : ''});
+  return `  socket.on("${key}", (${args.join(", ")}) => {
+    impl.${key}(state, userData${args.length ? [""].concat(args).join(", ") : ""});
     broadcastUpdates(stateId, state);
   });`;
+};
+
+const stringifyClientMethod = (key: string, value: any) => {
+  const args = value ?? {};
+  const argNames = Object.keys(args);
+  const argsStr = Object.entries(args)
+    .map(([name, type]) => `${name}: ${type}`)
+    .join(", ");
+  return `  public ${key}(${argsStr}): void {
+    this.socket.emit("${key}"${argNames.length ? [""].concat(argNames).join(", ") : ""});
+  }`;
 };
 
 const doc = safeLoad(readFileSync("src/types.yml", "utf8"));
@@ -35,21 +46,27 @@ Object.entries(doc.types).forEach(([key, value]) => {
 writeFileSync("src/generated/types.ts", typesOutput, "utf8");
 
 // server
-let methodOutput = "";
+let serverMethodOutput = "";
 Object.entries(doc.methods).forEach(([key, value]) => {
   if (key !== doc.initialize) {
-    methodOutput += stringifyMethod(key, value) + "\n";
+    serverMethodOutput += stringifyServerMethod(key, value) + "\n";
   }
 });
 const serverTemplate = readFileSync("server.ts.template", "utf8");
 const serverOutput = serverTemplate
-  .replace(/{{methods}}/g, methodOutput)
+  .replace(/{{methods}}/g, serverMethodOutput)
   .replace(/{{UserData}}/g, doc.userData);
 writeFileSync("src/generated/server.ts", serverOutput, "utf8");
 
 // client
+let clientMethodOutput = "";
+Object.entries(doc.methods).forEach(([key, value]) => {
+  if (key !== doc.initialize) {
+    clientMethodOutput += stringifyClientMethod(key, value) + "\n\n";
+  }
+});
 const clientTemplate = readFileSync("client.ts.template", "utf8");
-const clientOutput = clientTemplate;
+const clientOutput = clientTemplate.replace(/{{methods}}/g, clientMethodOutput);
 writeFileSync("src/generated/client.ts", clientOutput, "utf8");
 
 // app
