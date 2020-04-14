b97b190592d342faeb57460163b7cd886a5e83c3
dieseldeesh <adhishramkumar@yahoo.com>
2020-04-13 22:38:10 -0500

    some typing changes

--- generate.ts
+++ generate.ts
@@ -3,6 +3,7 @@ import { readFileSync, writeFileSync, existsSync, mkdirSync } from "fs";
 import { compile, registerHelper } from "handlebars";
 
 registerHelper("ne", (a, b) => a !== b);
+registerHelper("eq", (a, b) => a === b);
 registerHelper("isArray", Array.isArray);
 registerHelper("isObject", x => typeof x === "object");
 registerHelper("join", (params, joinStr, prepend, postpend, options) => {
@@ -13,34 +14,36 @@ registerHelper("join", (params, joinStr, prepend, postpend, options) => {
     const paramsStr = Object.entries(params || {})
       .map(([name, type]) => options.fn({ name, type }))
       .join(joinStr);
-    return (prepend && paramsStr.length ? joinStr : "") + paramsStr + (postpend && paramsStr.length ? joinStr : "");
+    return (
+      (prepend && paramsStr.length ? joinStr : "") +
+      paramsStr +
+      (postpend && paramsStr.length ? joinStr : "")
+    );
   }
 });
 registerHelper("getArgsInfo", (args: { [name: string]: string }) => {
-  return Object.entries(args).map(([name, type]) => {
-    if (type.endsWith("[]")) {
-      const primType = getPrimitiveTypeRecursively(type.substring(0, type.length - 2));
-      if (Array.isArray(primType)) {
-        return { name, type: "enum", multi: true, values: primType };
-      } else {
-        return { name, type: primType, multi: true };
-      }
-    } else {
-      const primType = getPrimitiveTypeRecursively(type);
-      if (Array.isArray(primType)) {
-        return { name, type: "enum", multi: false, values: primType };
-      } else {
-        return { name, type: primType, multi: false };
-      }
-    }
-  });
+  return Object.entries(args).map(([name, type]) => getType(name, type));
 });
 
-function getPrimitiveTypeRecursively(type: string | string[]): string | string[] {
-  if (type !== "string" && !Array.isArray(type)) {
-    return getPrimitiveTypeRecursively(doc.types[type]);
+function getType(name: string, type: string | string[] | { [name: string]: string }): any {
+  if (typeof type === "string") {
+    if (type.endsWith("[]")) {
+      return { name, type: "array", args: getType("", type.substring(0, type.length - 2)) };
+    }
+    if (type === "string" || type === "number" || type === "boolean") {
+      return { name, type };
+    } else {
+      return getType(name, doc.types[type]);
+    }
+  } else if (Array.isArray(type)) {
+    return { name, type: "enum", values: type };
+  } else {
+    return {
+      name,
+      type: "object",
+      args: Object.entries(([name, value]: any) => getType(name, value)),
+    };
   }
-  return type;
 }
 
 function generate(filename: string) {
@@ -49,6 +52,7 @@ function generate(filename: string) {
 }
 
 const doc = safeLoad(readFileSync("src/types.yml", "utf8"));
+console.log(doc.types);
 
 if (!existsSync("src/generated")) {
   mkdirSync("src/generated");
