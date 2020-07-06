f4cd4e494a77949a609e283c6e4bf83f088bd992
Kushal Nigam <kushal.nigam1@gmail.com>
2020-07-06 16:31:10 -0700

    added simple plugin example for RoleInfo[]

---
+++ examples/avalon/plugins/RoleInfos.ts
@@ -0,0 +1,42 @@
+import Vue from "vue";
+import { Role, RoleInfo } from "../.lsot/types";
+
+Vue.component("RoleInfos", {
+  data: function () {
+    return {
+      isOpen: true,
+    };
+  },
+  props: { value: Array },
+  template: `
+  <span v-if="value && value.length > 0">
+    <button class="button small" type="button" v-on:click="isOpen=!isOpen">
+      <span class="button-text" v-if="isOpen">-</span>
+      <span class="button-text" v-else>+</span>
+    </button>
+    <span v-if="!isOpen">...</span>
+    <table style="color: white" v-else>
+        <tr>
+            <th>Role</th>
+            <th>Is Evil?</th>
+            <th>Known Roles</th>
+            <th>Quantity</th>
+        </tr>
+        <tr v-for="v in value.filter(i => i.quantity > 0)">
+            <td>{{roleToString(v.role)}}</td>
+            <td>{{v.isEvil}}</td>
+            <td>{{formatKnownRoles(v.knownRoles, value.filter(i => i.quantity > 0))}}</td>
+            <td>{{v.quantity}}</td>
+        </tr>
+    </table>
+  </span>`,
+  methods: {
+    roleToString(r: Role) {
+      return Role[r].toLowerCase().replace(/^[a-z]/, (x) => x.toUpperCase());
+    },
+    formatKnownRoles(knownRoles: Role[], roleInfosInGame: RoleInfo[]) {
+      const rolesInGame = roleInfosInGame.map((i) => i.role);
+      return knownRoles.filter((r) => rolesInGame.includes(r)).map(this.roleToString);
+    },
+  },
+});
