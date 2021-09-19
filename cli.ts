de8a18c01167f775d72a2a115e7b52f2c66078b5
Harsh Pandey <hpandey793@gmail.com>
2021-09-19 01:10:50 -0400

    simplify dotenv setup

--- cli.ts
+++ cli.ts
@@ -292,9 +292,11 @@ if (command === "init") {
   }).then((server) => server.listen());
   shelljs.cd(serverDir);
   process.env.DATA_DIR = join(serverDir, ".rtag/data");
+  process.env.DOTENV_CONFIG_PATH = join(rootDir, ".env");
   process.env.NODE_LOADER_CONFIG = join(__dirname, "node-loader.config.mjs");
   const loaderPath = join(__dirname, "node_modules/@node-loader/core/lib/node-loader-core.js");
-  shelljs.exec(`node --loader ${loaderPath} --experimental-specifier-resolution=node .rtag/store.ts`, { async: true });
+  const storePath = join(serverDir, ".rtag/store.ts");
+  shelljs.exec(`node --loader ${loaderPath} --experimental-specifier-resolution=node ${storePath}`, { async: true });
 } else if (command === "build") {
   process.env.VITE_APP_ID = appId;
   build({
