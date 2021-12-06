109580a5db69bc9b162dbc5fd68d7892f615a83f
Harsh Pandey <hpandey793@gmail.com>
2021-12-06 01:03:50 -0500

    draw state on canvas

--- examples/pong/client/plugins/PlayerState.ts
+++ examples/pong/client/plugins/PlayerState.ts
@@ -3,11 +3,40 @@ import { property } from "lit/decorators.js";
 import { PlayerState } from "../.rtag/types";
 import { RtagConnection } from "../.rtag/client";
 
+const WIDTH = 600;
+const HEIGHT = 400;
+const PADDLE_WIDTH = 10;
+const PADDLE_HEIGHT = 50;
+const BALL_RADIUS = 10;
+
 export default class CardsComponent extends LitElement {
   @property() val!: PlayerState;
   @property() client!: RtagConnection;
+  ctx: CanvasRenderingContext2D | undefined;
 
   render() {
-    return html`<canvas width="600" height-"400"></canvas>`;
+    return html`<canvas width="${WIDTH}" height="${HEIGHT}"></canvas>`;
+  }
+
+  firstUpdated() {
+    const canvas = this.renderRoot.querySelector("canvas")!;
+    this.ctx = canvas.getContext("2d")!;
+  }
+
+  updated() {
+    const ctx = this.ctx!;
+    if (ctx === undefined) {
+      return;
+    }
+
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, this.val.paddleA - PADDLE_HEIGHT / 2, PADDLE_WIDTH, PADDLE_HEIGHT);
+    ctx.fillRect(WIDTH - PADDLE_WIDTH, this.val.paddleB - PADDLE_HEIGHT / 2, PADDLE_WIDTH, PADDLE_HEIGHT);
+    ctx.beginPath();
+    ctx.arc(this.val.ball.x, this.val.ball.y, BALL_RADIUS, 0, 2 * Math.PI);
+    ctx.fill();
   }
 }
