0253d443ce9c910be46253a9968d9048932de489
mihirpandya <mihir.m.pandya@gmail.com>
2022-01-19 14:14:10 -0800

    npm test

---
+++ run-all-examples.sh
@@ -0,0 +1,10 @@
+echo "Running all examples."
+cd examples;
+for example in `ls examples`; do
+  echo "Running $example";
+  cd $example;
+  ts-node ../../cli.ts;
+  if [ $? -ne 0 ]; then
+    echo "Failed to run example: $example";
+  fi;
+done
