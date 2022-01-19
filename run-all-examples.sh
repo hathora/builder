34dd436358164f38cceb7b65ad83ad34ee6b5a13
mihirpandya <mihir.m.pandya@gmail.com>
2022-01-19 14:19:42 -0800

    fix syntax

--- run-all-examples.sh
+++ run-all-examples.sh
@@ -1,14 +1,14 @@
 pwd
 echo "Running all examples."
 cd examples;
-success = 0;
+success=0;
 for example in `ls`; do
   echo "Running $example";
   cd $example;
   ts-node ../../cli.ts;
   if [ $? -ne 0 ]; then
     echo "Failed to run example: $example";
-    success = 1;
+    success=1;
   fi;
   cd ..
 done
