c10400e1712f1dc7318b3ef0895b6c25f2c65dbb
Harsh Pandey <hpandey793@gmail.com>
2022-02-03 21:41:02 -0500

    fix chat

--- examples/chat/server/impl.ts
+++ examples/chat/server/impl.ts
@@ -13,11 +13,11 @@ export class Impl implements Methods<RoomState> {
     return { name: request.name, createdBy: userId, messages: [] };
   }
   sendPublicMessage(state: RoomState, userId: UserId, ctx: Context, request: ISendPublicMessageRequest): Response {
-    state.messages.push({ text: request.text, sentAt: ctx.time(), sentBy: userId });
+    state.messages.push({ text: request.text, sentAt: ctx.time, sentBy: userId });
     return Response.ok();
   }
   sendPrivateMessage(state: RoomState, userId: UserId, ctx: Context, request: ISendPrivateMessageRequest): Response {
-    state.messages.push({ text: request.text, sentAt: ctx.time(), sentBy: userId, sentTo: request.to });
+    state.messages.push({ text: request.text, sentAt: ctx.time, sentBy: userId, sentTo: request.to });
     return Response.ok();
   }
   getUserState(state: RoomState, userId: UserId): RoomState {
