0d670eb7c424a53387df773576af3e37b92b222e
Harsh Pandey <hpandey793@gmail.com>
2022-02-04 14:24:58 -0500

    rework chat example

--- examples/chat/server/impl.ts
+++ examples/chat/server/impl.ts
@@ -4,26 +4,51 @@ import {
   UserId,
   RoomState,
   ICreateRoomRequest,
+  IJoinRoomRequest,
+  ILeaveRoomRequest,
   ISendPublicMessageRequest,
   ISendPrivateMessageRequest,
 } from "../api/types";
 
 export class Impl implements Methods<RoomState> {
   createRoom(userId: UserId, ctx: Context, request: ICreateRoomRequest): RoomState {
-    return { name: request.name, createdBy: userId, messages: [] };
+    return { createdBy: userId, users: [userId], messages: [] };
+  }
+  joinRoom(state: RoomState, userId: string, ctx: Context, request: IJoinRoomRequest): Response {
+    if (state.users.includes(userId)) {
+      return Response.error("Already joined");
+    }
+    state.users.push(userId);
+    return Response.ok();
+  }
+  leaveRoom(state: RoomState, userId: string, ctx: Context, request: ILeaveRoomRequest): Response {
+    if (!state.users.includes(userId)) {
+      return Response.error("Not joined");
+    }
+    state.users.splice(state.users.indexOf(userId), 1);
+    return Response.ok();
   }
   sendPublicMessage(state: RoomState, userId: UserId, ctx: Context, request: ISendPublicMessageRequest): Response {
+    if (!state.users.includes(userId)) {
+      return Response.error("Not joined");
+    }
     state.messages.push({ text: request.text, sentAt: ctx.time, sentBy: userId });
     return Response.ok();
   }
   sendPrivateMessage(state: RoomState, userId: UserId, ctx: Context, request: ISendPrivateMessageRequest): Response {
+    if (!state.users.includes(userId)) {
+      return Response.error("Not joined");
+    }
+    if (!state.users.includes(request.to)) {
+      return Response.error("Recpient not joined");
+    }
     state.messages.push({ text: request.text, sentAt: ctx.time, sentBy: userId, sentTo: request.to });
     return Response.ok();
   }
   getUserState(state: RoomState, userId: UserId): RoomState {
     return {
-      name: state.name,
       createdBy: state.createdBy,
+      users: state.users,
       messages: state.messages.filter(
         (msg) => msg.sentBy === userId || msg.sentTo === userId || msg.sentTo === undefined
       ),
