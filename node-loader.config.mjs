834d58b02ad0b5dc6f438b898d354b9f479a478b
Harsh Pandey <hpandey793@gmail.com>
2022-02-21 23:19:06 -0500

    disable hot reload on windows for now

--- node-loader.config.mjs
+++ node-loader.config.mjs
@@ -1,6 +1,7 @@
+import os from "os";
 import * as tsNode from "ts-node/esm";
 import * as hotEsm from "hot-esm";
 
 export default {
-  loaders: [hotEsm, tsNode],
+  loaders: os.platform() !== "win32" ? [hotEsm, tsNode] : [tsNode],
 };
