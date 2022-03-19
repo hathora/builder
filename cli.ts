cff40d3ded1c1d075cbe8bd8b37ccf320fca8dca
Arran France <arran@arranfrance.com>
2022-03-19 09:02:52 +0000

    reformat cli.ts and remove .nvmrc

--- cli.ts
+++ cli.ts
@@ -30,8 +30,7 @@ function getProjectRoot(cwd: string): string {
 function getAppConfig() {
   const appSecret = process.env.APP_SECRET ?? uuidv4();
   const appId = createHash("sha256").update(appSecret).digest("hex");
-  const coordinatorHost =
-    process.env.COORDINATOR_HOST ?? "coordinator.hathora.dev";
+  const coordinatorHost = process.env.COORDINATOR_HOST ?? "coordinator.hathora.dev";
   return { appId, appSecret, coordinatorHost };
 }
 
@@ -40,10 +39,7 @@ function generateLocal() {
   const appConfig = getAppConfig();
   generate(rootDir, "templates/base", appConfig);
   if (!existsSync(join(rootDir, ".env"))) {
-    outputFileSync(
-      join(rootDir, ".env"),
-      `APP_SECRET=${appConfig.appSecret}\n`
-    );
+    outputFileSync(join(rootDir, ".env"), `APP_SECRET=${appConfig.appSecret}\n`);
   }
 }
 
@@ -73,16 +69,11 @@ async function startServer() {
   shelljs.cd(join(serverDir, ".hathora"));
   process.env.DATA_DIR = join(rootDir, "data");
   process.env.NODE_LOADER_CONFIG = join(__dirname, "node-loader.config.mjs");
-  const loaderPath = pathToFileURL(
-    require.resolve("@node-loader/core/lib/node-loader-core.js")
-  );
+  const loaderPath = pathToFileURL(require.resolve("@node-loader/core/lib/node-loader-core.js"));
   const storePath = join(serverDir, ".hathora/store.ts");
-  const cp = shelljs.exec(
-    `node --loader ${loaderPath} --experimental-specifier-resolution=node ${storePath}`,
-    {
-      async: true,
-    }
-  );
+  const cp = shelljs.exec(`node --loader ${loaderPath} --experimental-specifier-resolution=node ${storePath}`, {
+    async: true,
+  });
   return new Promise((resolve, reject) => {
     cp.stdout?.on("data", resolve);
     cp.on("error", reject);
@@ -207,10 +198,7 @@ yargs(hideBin(process.argv))
     aliases: ["gamesave", "sv"],
     describe: "Creates a named save game from a specific state id",
     handler: (argv) => {
-      copySync(
-        join(rootDir, "data", argv.stateId as string),
-        join(rootDir, "data", "saves", argv.saveName as string)
-      );
+      copySync(join(rootDir, "data", argv.stateId as string), join(rootDir, "data", "saves", argv.saveName as string));
     },
   })
   .command({
