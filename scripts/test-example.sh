1da2c42df021e55f08aa0541bbf755e29466294f
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-04-16 01:14:05 +0200

    Allow testing a single example at a time

---
+++ scripts/test-example.sh
@@ -0,0 +1,23 @@
+example="$1";
+success=0;
+
+if [ -z $1 ]; then
+  echo "Usage: Provide the name of the example project you want to test.";
+  echo "\nAvailable examples:";
+  echo $(ls examples);
+  exit 1;
+fi;
+
+cd examples;
+
+echo "Testing $example...";
+cd $example;
+ts-node ../../cli.ts generate && \
+ ts-node ../../cli.ts install && \
+ npx tsc server/.hathora/store.ts --esModuleInterop --target esnext --module esnext --moduleResolution node --noEmit
+if [ $? -ne 0 ]; then
+  echo "Failed to run example: $example";
+  success=1;
+fi;
+cd ..;
+exit $success;
