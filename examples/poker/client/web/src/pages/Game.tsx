0b5abf81fdc3ef1126f54a96c6df03e967fabc01
Sami Shukri <samiziiv@gmail.com>
2022-05-06 11:51:51 -0600

    add ability to link to home

--- examples/poker/client/web/src/pages/Game.tsx
+++ examples/poker/client/web/src/pages/Game.tsx
@@ -1,10 +1,11 @@
 import { useHathoraContext } from "../context/GameContext";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import useAutoJoinGame from "../hooks/useAutoJoinGame";
 import Lobby from "../components/Lobby";
 import ActiveGame from "../components/ActiveGame";
 import { RoundStatus } from "../../../../api/types";
 import Loader from "../components/PageLoader";
+import Logo from "../assets/hathora-hammer-logo-light.png";
 
 export default function Game() {
   const { gameId } = useParams();
@@ -12,11 +13,32 @@ export default function Game() {
 
   useAutoJoinGame(gameId);
 
-  if (playerState?.roundStatus === RoundStatus.WAITING) {
-    return <Lobby status={playerState?.roundStatus} />;
-  } else if (playerState?.roundStatus === RoundStatus.ACTIVE || playerState?.roundStatus === RoundStatus.COMPLETED) {
-    return <ActiveGame />;
-  }
+  const RenderGame = () => {
+    if (playerState?.roundStatus === RoundStatus.WAITING) {
+      return <Lobby status={playerState?.roundStatus} />;
+    } else if (playerState?.roundStatus === RoundStatus.ACTIVE || playerState?.roundStatus === RoundStatus.COMPLETED) {
+      return <ActiveGame />;
+    }
 
-  return <Loader />;
+    return <Loader />;
+  };
+
+  return (
+    <div>
+      <div className="flex flex-row bg-slate-200 p-2 md:p-5">
+        <div className="flex flex-row justify-center items-center">
+          <Link to={"/"}>
+            <img src={Logo} style={{ height: 50 }} />
+          </Link>
+          <div>
+            Powered By{" "}
+            <strong>
+              <a href="https://github.com/hathora/hathora">Hathora</a>
+            </strong>
+          </div>
+        </div>
+      </div>
+      {RenderGame()}
+    </div>
+  );
 }
