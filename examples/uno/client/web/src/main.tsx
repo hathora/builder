c77ef3046cc0d29a8485c5f6d5bd22f3902a6957
Sami Shukri <samiziiv@gmail.com>
2022-05-06 21:26:48 -0600

    revert changes to main.tsx

--- examples/uno/client/web/src/main.tsx
+++ examples/uno/client/web/src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import HathuraContextProvider from "./context/GameContext";
+import "react-toastify/dist/ReactToastify.css";
 
 import App from "./App";
 import "./index.css";
-import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <HathuraContextProvider>
