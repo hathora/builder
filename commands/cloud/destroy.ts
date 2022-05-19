322d2eb91a2eed6616cc93f3df838710bcde2e0f
Siddharth Dhulipalla <sid@hathora.dev>
2022-05-19 17:52:41 -0400

    add --yes flag for destroy

--- commands/cloud/destroy.ts
+++ commands/cloud/destroy.ts
@@ -10,22 +10,29 @@ import prompts from "prompts";
 const cmd: CommandModule = {
   command: "destroy",
   describe: "Destroy a Hathora Cloud application",
-  builder: { appName: { type: "string", demandOption: true } },
+  builder: {
+    appName: { type: "string", demandOption: true },
+    yes: { type: "boolean", describe: "Accept all confirmations" },
+  },
   handler: async (argv) => {
     const tokenFile = join(os.homedir(), ".config", "hathora", "token");
     if (!existsSync(tokenFile)) {
       console.log(chalk.redBright(`Missing token file, run ${chalk.underline("hathora login")} first`));
       return;
     }
-    const userInput = await prompts({
-      type: "confirm",
-      name: "value",
-      message: `Are you sure you want to destroy ${argv.appName}? This action is irreverable.`,
-    });
+    if (!argv.yes) {
+      const userInput = await prompts({
+        type: "confirm",
+        name: "value",
+        message: `Are you sure you want to destroy ${argv.appName}? This action is irreversible.`,
+      });
 
-    if (!userInput.value) {
-      return;
+      if (!userInput.value) {
+        return;
+      }
     }
+    console.log('got here');
+    return;
     const token = fs.readFileSync(tokenFile).toString();
     const apps = await axios.delete(`https://cloud.hathora.com/app/${argv.appName}`, {
       headers: {
