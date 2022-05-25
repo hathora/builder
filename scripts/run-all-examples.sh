b4fac9811454a1696fed32f567f806b38c4ec25c
Harsh Pandey <hpandey793@gmail.com>
2022-05-25 11:04:13 -0400

    fix test

--- scripts/run-all-examples.sh
+++ scripts/run-all-examples.sh
@@ -4,8 +4,8 @@ success=0;
 for example in `ls`; do
   echo "Testing $example...";
   cd $example;
-  ts-node ../../cli.ts generate && \
-   ts-node ../../cli.ts install && \
+  ts-node ../../src/cli.ts generate && \
+   ts-node ../../src/cli.ts install && \
    npx tsc server/.hathora/store.ts --esModuleInterop --target esnext --module esnext --moduleResolution node --noEmit
   if [ $? -ne 0 ]; then
     echo "Failed to run example: $example";
