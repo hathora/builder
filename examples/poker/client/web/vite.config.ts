9b20caade5de71957f9c9f7072f2e18b5a3567cb
Harsh Pandey <hpandey793@gmail.com>
2022-06-07 14:12:39 -0400

    fix poker and uno

--- examples/poker/client/web/vite.config.ts
+++ examples/poker/client/web/vite.config.ts
@@ -1,7 +1,23 @@
+import { join, dirname, basename } from "path";
+import { fileURLToPath } from "url";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: join("..", "..", "dist", "client", basename(dirname(fileURLToPath(import.meta.url)))),
+    target: "esnext",
+    emptyOutDir: true,
+  },
+  define: {
+    "process.env": {
+      COORDINATOR_HOST: process.env.COORDINATOR_HOST,
+      MATCHMAKER_HOST: process.env.MATCHMAKER_HOST,
+    },
+  },
+  server: { host: "0.0.0.0" },
+  clearScreen: false,
 });
